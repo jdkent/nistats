a47586fc1326c86578fcf780768395f64b42c9f1
James Kent <james-kent@uiowa.edu>
2020-01-08 12:07:13 -0600

    attempt to address @bthirion comments

--- examples/02_first_level_models/plot_predictions_residuals.py
+++ examples/02_first_level_models/plot_predictions_residuals.py
@@ -18,6 +18,8 @@ from nilearn import plotting, masking
 from nistats.reporting import get_clusters_table
 from nistats.first_level_model import FirstLevelModel
 import pandas as pd
+import numpy as np
+
 
 # load fMRI data
 subject_data = fetch_spm_auditory()
@@ -115,6 +117,33 @@ plt.tight_layout()
 # Because we stored the residuals, we can plot the R-squared: the proportion
 # of explained variance of the GLM as a whole. Note that the R-squared is markedly
 # lower deep down the brain, where there is more physiological noise and we 
-# are further away from the receive coils.
+# are further away from the receive coils. However, R-Squared should be interpreted
+# with a grain of salt. The R-squared value will necessarily increase with
+# the addition of more factors (such as rest, active, drift, motion) into the GLM.
+# Additionally, we are looking at the overall fit of the model, so we are
+# unable to say whether a voxel/region has a large R-squared value because
+# the voxel/region is responsive to the experiment (such as active or rest)
+# or because the voxel/region fits the noise factors (such as drift or motion)
+# that could be present in the GLM. To isolate the influence of the experiment,
+# we can use an F-test as shown in the next section.
 plotting.plot_stat_map(fmri_glm.r_square, 
                        bg_img=mean_img, threshold=.1, display_mode='z', cut_coords=7)
+
+
+#########################################################################
+# Calculate and Plot F-test
+# -------------------------
+# The F-test tells you how well the GLM fits effects of interest such as 
+# the active and rest conditions together. This is different from R-squared,
+# which tells you how well the overall GLM fits the data, including active, rest
+# and all the other columns in the design matrix such as drift and motion.
+design_matrix = fmri_glm.design_matrices_[0]
+active = np.array([1 if c == 'active' else 0 for c in design_matrix.columns])
+rest = np.array([1 if c == 'rest' else 0 for c in design_matrix.columns])
+effects_of_interest = np.vstack((active, rest))
+
+z_map = fmri_glm.compute_contrast(effects_of_interest,
+                                  output_type='z_score')
+
+plotting.plot_stat_map(z_map, 
+                       bg_img=mean_img, threshold=2.33, display_mode='z', cut_coords=7)
